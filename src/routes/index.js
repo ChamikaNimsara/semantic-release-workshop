3fe82e1ce8c0722c0ef70caf235ecf8567c7f5e0
Chamika Bandaranayake <cnb@tiqri.com>
2026-02-26 18:36:30 +0530

    fix: updated status to message

--- src/routes/index.js
+++ src/routes/index.js
@@ -38,7 +38,7 @@ router.get('/test', (req, res) => {
 });
 
 router.get('/test2', (req, res) => {
-  res.json({ status: 'This is a test2 api', timestamp: new Date().toISOString() });
+  res.json({ message: 'This is a test2 api', timestamp: new Date().toISOString() });
 });
 
 module.exports = router;
